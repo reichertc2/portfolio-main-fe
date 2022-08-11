e6c6577dc4ede33f85568a5f507b36f8d5304ec0
Chris Reichert <chris.a.reichert@gmail.com>
2022-08-11 11:58:37 -0500

    added basic info to headline and about me pages

--- components/Bio/AboutMe.js
+++ components/Bio/AboutMe.js
@@ -1,7 +1,51 @@
-export default function AboutMe () {
+import Image from "next/image"
+import placeHolderImage from '../../public/placeholder-image.png'
+
+export default function AboutMe() {
     return (
-        <section>
+        <section className="py-6">
             <h3>About Me</h3>
+
+            <p>
+                Hello. My name is Chris and i enjoy creating and automating processes through the power of code. My ineterest began while I was in high school where I wrote my first HTML code.
+            </p>
+            <p>
+                I did step away from software to pursue a military career as an avaition officer and company commander. Were I trained in leadership and the AH-64D attack helicopter.
+            </p>
+            <p>
+                I then pursued a career in construction as a project manager earning my Project Management Professional (PMP).
+            </p>
+            <p>
+                I finally revisited programming and fell in love. I attended a coding training program at CodFellows, which I highly recommend.
+            </p>
+            <p>
+                Here are a few technologies and languages I am familiar:
+
+            </p>
+
+            <ul>
+                <li>Python</li>
+                <li>JavaScript</li>
+
+
+            </ul>
+            <ul>
+                <li>Django</li>
+                <li>Flask</li>
+                <li>React</li>
+                <li>Next.js</li>
+                <li>Node.js</li>
+
+            </ul>
+            <Image
+                className='rounded-full'
+
+                src={placeHolderImage}
+                alt='Placeholder'
+                width={100}
+                height={100}
+            />
+
         </section>
     )
 }
--- components/Bio/Headline.js
+++ components/Bio/Headline.js
@@ -1,7 +1,11 @@
-export default function Headline () {
+export default function Headline() {
     return (
-        <section>
-            <h3>Headline</h3>
+        <section className="py-6">
+            <h6>Hi, my name is </h6>
+            <h2>Chris Reichert.</h2>
+            <h2>I build things for the web.</h2>
+            <p>Software Engineer | Python | JavaScript | Veteran | PMP
+            </p>
         </section>
     )
 }
