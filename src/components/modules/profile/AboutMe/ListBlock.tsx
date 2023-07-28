3264bf1b2b05a6d9be2c56003bc4cce2eec872fa
reichertc2 <chris.a.reichert@gmail.com>
2023-07-28 16:15:14 -0400

    another possible fix

--- src/components/modules/profile/AboutMe/ListBlock.tsx
+++ src/components/modules/profile/AboutMe/ListBlock.tsx
@@ -43,7 +43,7 @@ export const ListBlock: React.FC<IListBlockProps> = ({ title, listProps, orienta
     useEffect(() => {
         checkOrientation()
 
-    }, [orientation])
+    })
 
     return (
 
