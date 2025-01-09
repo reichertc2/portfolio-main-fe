f9a1e09368c45c76e30412f51a5d473bc6ce6057
reichertc2 <chris.a.reichert@gmail.com>
2025-01-09 05:54:04 -0600

    corrected build error

--- src/app/(public)/Articles/page.tsx
+++ src/app/(public)/Articles/page.tsx
@@ -1,14 +1,12 @@
 import MainContainer from "@/app/_components/common/MainContainer";
 import ArticlesHome from "@/app/_components/modules/articles/ArticlesHome";
-import { theme } from "../../../../data/theme";
-
 
 interface IArticlePageProps {}
 
 const ArticlesPage: React.FC<IArticlePageProps> = () => {
   return (
     <MainContainer>
-      <ArticlesHome theme={theme} />
+      <ArticlesHome  />
     </MainContainer>
   );
 };
